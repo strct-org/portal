ea6e79f741bd901defafb4edeec11bd2f2da5656
Martbul <martbul01@gmail.com>
2026-01-23 08:44:07 +0200

    fix: remove next static config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
-  images: {
-    unoptimized: true,
-  },
-
+  reactStrictMode: true,
   trailingSlash: true,
 };
 
